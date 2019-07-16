821d6615d138cd0cd8690b2fa366ca2fee5f0f9e
jeimygarcia <jeimyg3004@gmail.com>
2019-07-15 20:19:30 -0400

    agregando autoridad

--- grails-app/init/sweetly/BootStrap.groovy
+++ grails-app/init/sweetly/BootStrap.groovy
@@ -1,20 +1,33 @@
 package sweetly
 
+import sweetly.Role
+import sweetly.User
+//import org.apache.shiro.crypto.hash.Sha256Hash
+import sun.security.provider.SHA
+
 class BootStrap {
 
      def init = {
           def usu=User.findByUsername('admin')
             if(!usu){
-              def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+        // def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+             def adminRole = new Role (name : 'ADMIN_ROLE')
+                adminRole.setAuthority()
+                adminRole.save()
 
                 def adminUser = new User(username: 'admin', password: '123456').save()
+               // def admin = new User(username: 'admin', passwordHash: new SHA("123456").toHex())
+              //  def adminUser = new User(username: 'admin', password: '123456').save()
+                adminUser.addTo(adminRole)
+                adminUser.save()
 
-                UserRole.create adminUser , adminRole
+                // def user = new User(username: "user", passwordHash: new 123456("user").toHex())
+              //  UserRole.create adminUser , adminRole
 
-               UserRole.withSession {
+              /* UserRole.withSession {
                     it.flush()
                     it.clear()
-                }
+                }*/
             }
 
             /*assert User.count() == 1
